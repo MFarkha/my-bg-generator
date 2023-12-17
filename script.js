6f040b2164560a48f36b071e7f7785d55a26adb9
Farhad Maten <farhad.maten@gmail.com>
2023-12-17 15:13:58 -0500

    added browserify support

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
-var body = document.getElementById('gradient');
-var input1 = document.getElementsByClassName("input1")[0];
-var input2 = document.getElementsByClassName("input2")[0];
-var h3 = document.getElementsByTagName('h3')[0];
+// import { without } from 'lodash';
 
-// console.log(input1);
-// console.log(input2);
-// console.log(h3);
+// console.log(without);
+
+var _ = require('lodash');
+
+console.log(_);
+
+let body = document.getElementById('gradient');
+let input1 = document.getElementsByClassName("input1")[0];
+let input2 = document.getElementsByClassName("input2")[0];
+let h3 = document.getElementsByTagName('h3')[0];
 
 function set_gradient(event) {
-    // console.log(event.target.value);
-    // console.log(input1.value);
     body.style.background = "linear-gradient(to right" + "," + input1.value + "," + input2.value + ")";
 
     h3.textContent = body.style.background;
 }
-
 
 input1.addEventListener('input', set_gradient)
 input2.addEventListener('input', set_gradient)
